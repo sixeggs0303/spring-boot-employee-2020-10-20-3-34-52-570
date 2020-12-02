50ffa1625e61cc75fa8b1e8d2c9d997605c7b3c2
sixeggs0303 <45088900+sixeggs0303@users.noreply.github.com>
2020-12-02 19:34:35 +0800

    [SpringBootPractice][Marcus Luk]test & feat: Added test case for getting all companies. Created company controller/service/repository.

---
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyServiceTest.java
@@ -0,0 +1,36 @@
+package com.thoughtworks.springbootemployee;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CompanyServiceTest {
+    @Test
+    public void should_return_all_companies_when_get_all_given_all_companies() {
+        //given
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(new Employee(1, "Marcus", 22, "male", 10000));
+        employeeList.add(new Employee(2, "Theo", 22, "male", 10));
+
+        CompanyRepository companyRepository = new CompanyRepository();
+        CompanyService companyService = new CompanyService(companyRepository);
+
+        List<Company> expected = new ArrayList<>();
+        expected.add(new Company(1, "Google", employeeList.size(), employeeList));
+        expected.add(new Company(2, "Facebook", employeeList.size(), employeeList));
+        expected.forEach(companyRepository::create);
+
+        //when
+        List<Company> companies = companyService.getCompanies();
+
+        //then
+        assertEquals(expected, companies);
+    }
+}
